092722a6d6beeed8210370c7106537716e41ce49
Pushkar Raj Johrani <pjohrani96@yahoo.com>
2019-01-24 16:28:35 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Company</title>
+</head>
+<style>
+	body{
+		padding-left: 100px;
+		padding-right: 100px;
+	}
+	.headerbg{
+		background-image: url("header.jpg");
+		background-repeat: no-repeat;
+		background-size: cover;
+		height: 100%;
+	}
+	.colbg{
+		background-color: red;
+	}
+	.card{
+		width: 30%;
+  border-radius: 4px;
+  border:1px solid #46ceef;
+  float: left;
+
+  margin: 0px 15px 0px 0px;
+}
+.card-title{
+  color: #46ceef;
+}
+.card-header{
+  background-color: #a5a3a3;
+}
+.side{
+	margin: 0px 0px 0px 0px;
+	width: 230px;
+	height: auto;
+	float: left;
+	border: 0px solid #33CC00;
+	
+}
+.news{
+	margin: 0px 0px 0px 0px;
+	width: 900px;
+	height: auto;
+	float: left;
+	border: 0px solid #33CC00;
+}
+.footer{
+    margin: 0px 0px 10px 0px;
+    width: 100%;
+    padding: 0;
+    height: auto;
+    float: left;
+    background-color: green;
+}
+ul{
+	list-style: none;
+}
+li{
+	margin: 0px 0px 10px 0px;
+}
+a{
+	
+	text-decoration: none;
+
+}
+</style>
+<body>
+	<div style="height: 130px;">
+		<div style="width: 20%; float: left;" align="center"><img src="LogoDTCopy.png" alt="logo" width="50%"></div>
+		<div class="headerbg" style="width: 80%; float: right; " align="center"><p style="color: white; font-family: sans-serif; margin-top: 50px;">High Education</p></div>
+	</div>
+	<div style="width: 100%;height: all; background-color: green" align="center">
+		<h2>There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain...</h2>
+    </div>
+
+    <div class="side">
+    	<aside>
+    		<ul>
+            	<li><a href="#">hi</a></li>
+                <li><a href="#">hi</a></li>
+                <li><a href="#">hi</a></li>
+                <li><a href="#">hi</a></li>
+            </ul>
+        </aside>
+   	</div>
+
+   	<div class="news">
+   		<div class="card" >
+   			<div class="card-header">Header 
+   				<hr>
+   			</div>
+   			<div>
+   				<h4>Primary card title</h4>
+   				<p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+   			</div>
+   		</div>
+   		<div class="card" >
+   			<div class="card-header">Header 
+   				<hr>
+   			</div>
+   			<div>
+   				<h4>Primary card title</h4>
+   				<p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+   			</div>
+   		</div>
+   		<div class="card" >
+   			<div class="card-header">Header 
+   				<hr>
+   			</div>
+   			<div>
+   				<h4>Primary card title</h4>
+   				<p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+   			</div>
+   		</div>
+   			<h2>What is Lorem Ipsum?</h2>
+   			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+   			<h3>Why do we use it?</h3>
+   			<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+   			<h3>Where does it come from?</h3>
+   			<p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
+   			<p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
+   	
+   	</div>
+
+    <div class="footer" align="center">
+		<h4>There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain...</h4>
+    </div>
+    <div class="footer" align="center">
+		<h4>There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain...</h4>
+    </div>
+  
+  
+</body>
+</html>
